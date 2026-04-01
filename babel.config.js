5c9461c10598cf06df1ad3b9d48359ec184ba779
Alpheus <26622568+alpheusmtx@users.noreply.github.com>
2026-04-01 08:16:44 +0800

    refactor: updates for babel config

---
+++ babel.config.js
@@ -0,0 +1,37 @@
+/** @param {import('@babel/core').ConfigAPI} api */
+module.exports = (api) => {
+    api.cache(true);
+
+    return {
+        presets: [
+            "babel-preset-expo",
+        ],
+        overrides: [
+            {
+                test: [
+                    /node_modules\/gqty\//,
+                    /node_modules\/@gqty\/react\//,
+                ],
+                plugins: [
+                    /**
+                     * @gqty/react uses `super(...arguments)`, so Babel needs the
+                     * class transform in the same override before the spread one.
+                     */
+                    "@babel/plugin-transform-classes",
+                    /**
+                     * Keep iterable spreads spec-correct for web production.
+                     *
+                     * GQty's query builder spreads Map/iterator values,
+                     * and loose mode rewrites those into broken Array.concat calls.
+                     */
+                    [
+                        "@babel/plugin-transform-spread",
+                        {
+                            loose: false,
+                        },
+                    ],
+                ],
+            },
+        ],
+    };
+};
