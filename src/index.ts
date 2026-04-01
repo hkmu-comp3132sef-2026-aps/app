467679bf8760aa0ad3a1aad0659e584583a5008e
Alpheus <26622568+alpheusmtx@users.noreply.github.com>
2026-04-01 15:57:47 +0800

    fix: add message channel polyfill

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,8 @@
+// Polyfills
 import "core-js/stable";
+// Expo Router generator support
 import "regenerator-runtime/runtime";
+// GQty MessageChannel API support
+import "message-channel-api/polyfill";
 
 import "expo-router/entry";
